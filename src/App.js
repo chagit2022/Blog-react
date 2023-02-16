3933bbd1b8e9b5a67a5ae65eabdd6a07d3475423
chagit2022 <chadoutaud.git@outlook.fr>
2023-02-16 13:18:39 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Navbar from "./Components/Navbar/Navbar"
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
     </div>
   );
 }
